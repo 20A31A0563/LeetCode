83c4f88d0a5ef405428734b5c218dba2d1ee5eb0
V SURYA SATISH <94625624+20A31A0563@users.noreply.github.com>
2023-07-23 15:56:42 +0530

    Added solution - LeetHub

---
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..py
@@ -0,0 +1,115 @@
+#User function Template for python3
+'''
+	Your task is to segregate the list of 
+	0s,1s and 2s.
+	
+	Function Arguments: head of the original list.
+	Return Type: head of the new list formed.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+
+'''
+class Solution:
+    #Function to sort a linked list of 0s, 1s and 2s.
+    def segregate(self, head):
+        l =[]
+        temp= head
+        while temp!=None:
+            l.append(temp.data)
+            temp = temp.next
+        i =0 
+        j= len(l)-1
+        k = 0
+        c =0
+        #print(l)
+        while(i<=j):
+            if l[i] ==0:
+                l[i],l[k] = l[k],l[i]
+                k=k+1
+                i=i+1
+            elif l[i]==2:
+                l[i] ,l[j] = l[j],l[i]
+                j=j-1
+            else:
+                i=i+1
+            #print(l,i)
+        #print(l)
+            
+        temp = head
+        i=0
+        while temp!=None:
+            temp.data = l[i]
+            temp=temp.next
+            i=i+1
+        return head
+        #code here
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+        
+# prints the elements of linked list starting with head
+def printList(head):
+    if head is None:
+        print(' ')
+        return
+    curr_node = head
+    while curr_node:
+        print(curr_node.data,end=" ")
+        curr_node=curr_node.next
+    print()
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        printList(Solution().segregate(a.head))
+# } Driver Code Ends
